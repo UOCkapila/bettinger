dca75005503bf0128b7c40ac01366f571db1be1c
Kapila Ranasinghe <kapila.ranasinghe@wheeve.co.uk>
2017-04-10 18:51:53 +0530

    add resource folder

---
+++ Server/src/main/java/bettnger/models/User.java
@@ -0,0 +1,22 @@
+package bettnger.models;
+
+import java.util.Date;
+
+/**
+ * Created by Kapila on 4/6/2017.
+ */
+public class User {
+
+    private int UserId;
+    private String firstNname;
+    private String lastName;
+    private Date dob;
+    private int age;
+    private int town_id;
+    private int nationality_id;
+    private int religion_id;
+    private int contact;
+    private String email;
+    private int Marital_Status_id;
+    private int photo_id;
+}
